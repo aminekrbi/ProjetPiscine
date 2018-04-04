61c5ce3dbe2c807e987ccdaabd00057208bf722a
maxzerrrrrr <34354306+maxzerrrrrr@users.noreply.github.com>
2018-04-04 15:38:57 +0200

    perte

--- graph_lib_v1/graph_lib_exemple_graph/graph.cpp
+++ graph_lib_v1/graph_lib_exemple_graph/graph.cpp
@@ -136,16 +136,16 @@ void Edge::post_update()
 /// éléments qui seront ensuite ajoutés lors de la mise ne place du Graphe
 GraphInterface::GraphInterface(int x, int y, int w, int h)
 {
-    m_top_box.set_dim(1000,740);
+    //m_top_box.set_dim(1000,740);
     m_top_box.set_gravity_xy(grman::GravityX::Right, grman::GravityY::Up);
 
-    m_top_box.add_child(m_tool_box);
+   /* m_top_box.add_child(m_tool_box);
     m_tool_box.set_dim(80,720);
     m_tool_box.set_gravity_xy(grman::GravityX::Left, grman::GravityY::Up);
-    m_tool_box.set_bg_color(BLANCBLEU);
+    m_tool_box.set_bg_color(BLANCBLEU);*/
 
     m_top_box.add_child(m_main_box);
-    m_main_box.set_dim(908,720);
+    m_main_box.set_dim(SCREEN_W,SCREEN_H);
     m_main_box.set_gravity_xy(grman::GravityX::Right, grman::GravityY::Up);
     m_main_box.set_bg_color(BLANCJAUNE);
 }
--- graph_lib_v1/graph_lib_exemple_graph/main.cpp
+++ graph_lib_v1/graph_lib_exemple_graph/main.cpp
@@ -2,9 +2,34 @@
 #include <iostream>
 
 #include "graph.h"
+affichagegraph(Graph g, int choixgraph)
+{
+    int it=choixgraph;
+    if (choixgraph<4)
+    {
+        while (!key[KEY_ESC] && choixgraph==it)
+        {
+            g.update();
+            grman::mettre_a_jour();
 
+            if (mouse_b&1 && (mouse_x<100 && mouse_x>15) && (mouse_y<100 && mouse_y>15))
+                {
+                    choixgraph--;
+                    return choixgraph;
+                }
+            if (mouse_b&1 && (mouse_x<1000 && mouse_x>915) && (mouse_y<100 && mouse_y>15))
+                {
+                    choixgraph++;
+                    return choixgraph;
+                }
+        }
+    }
+}
 int main()
 {
+    ///variables
+
+    int choixgraph=2;
     /// A appeler en 1er avant d'instancier des objets graphiques etc...
     grman::init();
 
@@ -22,9 +47,18 @@ int main()
     {
         /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
         g.update();
-
+            if(mouse_b&1 && (mouse_x<100 && mouse_x>15) && (mouse_y<100 && mouse_y>15))
+            {
+                choixgraph--;
+                choixgraph=affichagegraph(g,choixgraph);
+            }
+            if(mouse_b&1 && (mouse_x<1000 && mouse_x>915) && (mouse_y<100 && mouse_y>15))
+            {
+                   choixgraph++;
+                   choixgraph=affichagegraph(g,choixgraph);
+            }
         /// Mise à jour générale (clavier/souris/buffer etc...)
-        grman::mettre_a_jour();
+        //grman::mettre_a_jour();
     }
 
     grman::fermer_allegro();
